173d376839b5999bc7335468c6c8cf5b68d0ff39
igachmov <ivan.gachmov@abv.bg>
2016-10-10 00:18:22 +0300

    Homework - Loops with last * Task

--- Loops-Homework/Task8/Program.cs
+++ Loops-Homework/Task8/Program.cs
@@ -8,9 +8,61 @@ namespace Task8
 {
     class Program
     {
+        //public static int ForLoop(int result)
+        //{
+        //    int number = result;
+        //    for (int i = 1; i < result; i++)
+        //    {
+
+        //        number = number * i;
+
+        //    }
+
+        //    return number;
+        //}
         static void Main(string[] args)
         {
             //Напишете програма, която пресмята N!*K!/(N-K)! за дадени N и K. *
+            //First Version
+            //Console.Write("Enter N!: ");
+            //int N = int.Parse(Console.ReadLine());
+            //ForLoop(N);
+            //Console.Write("Enter K!: ");
+            //int K = int.Parse(Console.ReadLine());
+            //ForLoop(K);
+            //ForLoop(N - K);
+            //Console.WriteLine("N!*K!/(N-K)!= " + (ForLoop(N) *ForLoop(K)/ForLoop(N-K)));
+
+            //Second Version
+            Console.Write("Enter N!: ");
+            int N = int.Parse(Console.ReadLine());
+            int result = N;
+
+            for (int i = 1; i < N; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine("N!= " + result);
+
+            Console.Write("Enter K!: ");
+            int K = int.Parse(Console.ReadLine());
+            int result1 = K;
+
+            for (int i = 1; i < K; i++)
+            {
+                result1 = result1 * i;
+            }
+            Console.WriteLine("K!= " + result1);
+
+            int result2 = (N - K);
+            for (int i = 1; i < (N - K); i++)
+            {
+                result2 = result2 * i;
+            }
+            Console.WriteLine("(N-K)!= " + result2);
+
+            Console.WriteLine("N!*K!/(N-K)!= " + ((result * result1) / result2));
+
 
         }
     }
